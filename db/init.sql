923e2d8c4cfc3559c80943e78d80cf558fe505f0
isabellaperezcav <156531080+isabellaperezcav@users.noreply.github.com>
2024-04-23 21:05:49 -0500

    Piccoling

---
+++ db/init.sql
@@ -0,0 +1,41 @@
+create database piccoling;
+use piccoling
+
+CREATE TABLE facturas (
+    id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombreCliente VARCHAR(50),
+    emailCliente VARCHAR(50),
+    totalCuenta DECIMAL(10,2),
+    fecha DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE inventario (
+  id_inventario INT AUTO_INCREMENT,
+  ingrediente VARCHAR(100),
+  cantidad_disponible INT,
+  PRIMARY KEY (id_inventario)
+);
+
+CREATE TABLE menu (
+    id_menu int auto_increment,
+    nombre VARCHAR(100),
+    precio DECIMAL(10,2),
+    cantidad INT(11)
+);
+
+CREATE TABLE preparacion (
+    preparacionid INT PRIMARY KEY AUTO_INCREMENT,
+    menu_id INT,
+    inventario_id INT,
+    FOREIGN KEY (menu_id) REFERENCES menu(id_menu),
+    FOREIGN KEY (inventario_id) REFERENCES inventario(id_inventario)
+) ENGINE=InnoDB;
+
+CREATE TABLE usuarios (
+  nombrecompleto VARCHAR(90),
+  correo VARCHAR(80),
+  usuario VARCHAR(15) PRIMARY KEY,
+  clave VARCHAR(50)
+);
+
+INSERT INTO usuarios (nombrecompleto, correo, usuario, clave) VALUES ('Administrador', 'administrador@piccoling.com', 'admin', '1234');
