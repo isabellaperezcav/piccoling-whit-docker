65a286ad1be5c389c6e58b2256df5763eaf90390
isabellaperezcav <156531080+isabellaperezcav@users.noreply.github.com>
2024-05-10 11:19:17 -0500

    Piccoling

--- webPiccoling2/admin-menuPicco.php
+++ webPiccoling2/admin-menuPicco.php
@@ -11,7 +11,7 @@ if ($us == "") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Piccoling</title>
+    <title>Admin Piccoling-2</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
